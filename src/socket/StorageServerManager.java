fa116c5f1528076f0d22c1a0ddaa5dfdae424a3c
Abhineet1144 <mariobrain61@gmail.com>
2026-02-08 20:04:10 +0530

    Enhancements

--- src/socket/StorageServerManager.java
+++ src/socket/StorageServerManager.java
@@ -1,13 +1,22 @@
 package socket;
 
-public class StorageServerManager {
-    private static final StorageServerManager INSTANE = new StorageServerManager();
+import util.SocketIO;
 
-    public static StorageServerManager getInstance() {
-        return INSTANE;
+import java.io.IOException;
+import java.net.Socket;
+
+public class StorageServerManager {
+    private StorageServerManager() {
+
     }
 
-    public void init() {
+    public static StorageServerManager getInstance() {
+        return new StorageServerManager();
+    }
 
+    public SocketIO initSockets(String ipport) throws IOException {
+        String[] ipPort = ipport.split(":");
+        Socket socket = new Socket(ipPort[0], Integer.parseInt(ipPort[1]));
+        return new SocketIO(socket);
     }
 }
