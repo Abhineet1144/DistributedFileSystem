ce2aeb7f564393bc8f27f503a7f50ade0c7621d3
Abhineet1144 <mariobrain61@gmail.com>
2026-02-08 19:58:19 +0530

    Add root DIR Instance

--- src/meta/FileMeta.java
+++ src/meta/FileMeta.java
@@ -1,15 +1,17 @@
 package meta;
 
 public class FileMeta {
+    public static final FileMeta ROOT = new FileMeta("root", "folder", 0, 0L, "", "", null);
+
     private String fileName;
     private String type;
     private long size;
-    private String date;
+    private long date;
     private String mainOwner;
     private String storedPath;
     private FileMeta parent;
 
-    public FileMeta(String fileName, String type, int size, String date, String mainOwner, String storedPath, FileMeta parent) {
+    public FileMeta(String fileName, String type, int size, long date, String mainOwner, String storedPath, FileMeta parent) {
         this.fileName = fileName;
         this.type = type;
         this.size = size;
