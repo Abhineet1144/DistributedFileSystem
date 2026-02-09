4476e575ac3c5c6ff72e29b950b3b423d0409561
Tejzs <danduhman7@gmail.com>
2026-02-09 02:57:15 -0400

    bug fixes for storage server

--- src/socket/StorageRequestHandler.java
+++ src/socket/StorageRequestHandler.java
@@ -2,11 +2,14 @@ package socket;
 
 import util.SocketIO;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
 public class StorageRequestHandler implements Runnable {
     private SocketIO operationsRequester;
+    String absoluteRootPath = "/media/tejas/BACKUP/DistributedFileSystem/";
 
     public StorageRequestHandler(Socket requester) throws IOException {
         this.operationsRequester = new SocketIO(requester);
@@ -16,7 +19,24 @@ public class StorageRequestHandler implements Runnable {
     public void run() {
         try {
             while (true) {
-                System.out.println("Received: " + operationsRequester.receiveText());
+                String operation = operationsRequester.receiveText();
+                System.out.println("Received: " + operation);
+
+                switch (operation) {
+                    case "check-available":
+                        operationsRequester.sendText("ok");
+                        break;
+                    case "file-creation":
+                        String id = operationsRequester.receiveText();
+                        long len = operationsRequester.getStreamSize();
+                        operationsRequester.sendText("ok");
+                        File file = new File(id+".dat");
+                        try (FileOutputStream fos = new FileOutputStream(file)) {
+                            operationsRequester.receiveInputStream(fos, len);
+                        }
+                        operationsRequester.sendText("ok");
+                        break;
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
